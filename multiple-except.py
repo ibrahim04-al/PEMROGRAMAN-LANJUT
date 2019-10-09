7f89bcf5af59fc6e24d48fd2c9bf61e5c4e666f2
ibrahim04-al <55285989+ibrahim04-al@users.noreply.github.com>
2019-10-09 15:44:15 +0800

    Create multiple-except.py

---
+++ multiple-except.py
@@ -0,0 +1,26 @@
+def main():
+
+    print("PROGRAM PEMBAGIAN BILANGAN")
+
+    try:
+        a = float(input("masukkan a: "))
+        b = float(input("masukkan b: "))
+
+        hasil = a / b
+
+    except ZeroDivisionError:
+        print("\nERROR: Nilai b tidak boleh nol")
+
+    except ValueError:
+        print("\nERROR: a dan b harus berupa angka")
+
+    except KeyboardInterrupt:
+        print("\nERROR: Jangan tekan Ctrl+C!")
+
+    else:
+        print("\na : ", a)
+        print("b : ", b)
+        print("a / b = ", hasil)
+
+if __name__ == "__main__":
+    main()
