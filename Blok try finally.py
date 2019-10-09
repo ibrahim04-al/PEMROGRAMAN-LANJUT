842fbf33d9fcb7b6fd8b89f1a073d52befed98b0
ibrahim04-al <55285989+ibrahim04-al@users.noreply.github.com>
2019-10-09 15:42:58 +0800

    Create Blok try finally.py

---
+++ Blok try finally.py
@@ -0,0 +1,14 @@
+def main():
+    try:
+        f = open("data.txt", "w")
+
+        try:
+            f.erite("pemrograman python")
+        finally:
+            f.close()
+    except IOError:
+        print("\nERROR: File tidak dapat " +
+              "dibuka/ditulis")
+
+if __name__ == "__main__":
+    main()
